e06081326893ee7904b96d7bb6d2f04d225f4c8d
wb-n <wb55033@gmail.com>
2024-08-21 10:43:01 +0300

    wow

--- churchsystem.py
+++ churchsystem.py
@@ -3,45 +3,35 @@ bills = 0.30
 allowance = 0.40
 member_needs = 0.20
 miscellaneous = 0.10
-bills_amount = input("Enter total for electricity and water bill amount")
-Member_need = True
+bills_amount = int(input("Enter total for electricity and water bill amount: "))
 
 # The total amount of money collected
-total_collected = input("Total Offerings")
+total_collected = int(input("Total Offerings: "))
 
 # Calculate the amount collected for bills
 bills_collected = bills * total_collected
 print(f"Amount collected for bills: Ksh.{bills_collected}")
 
+# Calculate the excess bill balance if the amount collected for bills is less than or equal to the bills amount
+def calculate_excess_balance():
+    if bills_amount <= bills_collected:
+        excess_bill_balance = bills_collected - bills_amount
+    else:
+        print("No Excess bill balance")
+    return excess_bill_balance
+excess = calculate_excess_balance()
+print(f"Excess balance for bills is : Ksh.{excess}")
+
 # Calculate the amount collected for the allowance
 allowance_collected = allowance * total_collected
-print(f"Amount collected for allowance: Ksh.{allowance_collected}")
+print(f"Amount collected for Bishop: Ksh.{allowance_collected}")
 
 # Calculate the amount collected for member needs
 member_needs_collected = member_needs * total_collected
 print(f"Amount collected for member needs: Ksh.{member_needs_collected}")
+def member_needs_collected():
+
 
 # Calculate the amount collected for miscellaneous
 miscellaneous_collected = miscellaneous * total_collected
 print(f"Amount collected for miscellaneous: Ksh.{miscellaneous_collected}")
-
-def allocation():
-    return {
-        "Bills": bills_collected,
-        "Allowance": allowance_collected,
-        "Member Needs": member_needs_collected,
-        "Miscellaneous": miscellaneous_collected,
-        "Total Collected": total_collected
-    }
-
-def excess_bill():
-    while True:
-        if bills_amount < bills:
-            excess_bill_balance = bills - bills_amount
-            print(f"Excess bill balance: Ksh.{excess_bill_balance}")
-        else:
-            print("No Excess bill balance")
-    return excess_bill
-
-# Call the functions
-print(total_collected)
